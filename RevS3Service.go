e59b74bf820c868349d15f54d17798cb6f9e0837
Joseph Nam <josephknam@gmail.com>
2016-11-21 14:33:23 -0500

    better error handling

--- RevS3Service.go
+++ RevS3Service.go
@@ -13,6 +13,12 @@ import (
 
 var service = s3.New(session.New(&aws.Config{Region: aws.String("us-east-1")}))
 
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func listFiles(bucket string) {
 	params := &s3.ListObjectsV2Input{
 		Bucket: aws.String(bucket), // Required
@@ -28,19 +34,13 @@ func listFiles(bucket string) {
 		*/
 	}
 	response, err := service.ListObjectsV2(params)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	check(err)
 	fmt.Println(response)
 }
 
 func uploadFile(bucket string, file string) {
 	dat, err := ioutil.ReadFile(file)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	check(err)
 	bytesToSend := bytes.NewReader(dat)
 	params := &s3.PutObjectInput{
 		Bucket:        aws.String(bucket),
@@ -56,16 +56,13 @@ func uploadFile(bucket string, file string) {
 	service.PutObject(params)
 }
 
-func downloadFile(bucket string, file string) {
+func downloadFile(bucket string, file string) error {
 	params := &s3.GetObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(file + ".mp3"),
 	}
 	result, err := service.GetObject(params)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	check(err)
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(result.Body)
 	fileErr := ioutil.WriteFile(file+"_new.mp3", buf.Bytes(), 0644)
